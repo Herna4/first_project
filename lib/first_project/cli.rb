56b378c9fb8646914feb23817c96c9c9faa5f6f2
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-09 15:28:38 +0000

    commit

--- lib/first_project/cli.rb
+++ lib/first_project/cli.rb
@@ -1,7 +1,38 @@
 class FirstProject::CLI
 
   def call
-    puts "Welcome"
+    list_courses
+    menu
+    byebye
+  end
+
+  def list_courses
+    puts "Welcome to our environmment"
+    puts <<-DOC.gsub /^\s*/, ''
+      1. Education -Still available!
+      2. Business Administration -Still available!
+      3. Computer Science -Still available!
+      4. Health Science -Still available!
+    DOC
+
+  end
+
+  def menu
+    puts "Enter the number of course you would like more info on:"
+    input = gets.strip
+
+    case input
+    when "1"
+      puts "More info about course 1..."
+    when "2"
+      puts "More info about course 2..."
+    when "3"
+      puts "More info about course 3..."
+    when "4"
+      puts "More info about course 4..."
+  end
+
+  def byebye
   end
 
 end
