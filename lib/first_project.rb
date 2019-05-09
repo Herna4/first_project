1eb83cb9f5431992931f549345478d9b989b8545
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-09 18:55:51 +0000

    commit

--- lib/first_project.rb
+++ lib/first_project.rb
@@ -1,3 +1,8 @@
+# require 'nokogiri'
+# require 'open-uri'
+require 'pry'
+
+
 require_relative "./first_project/version"
 require_relative './first_project/scraper'
 require_relative './first_project/cli'
--- lib/first_project/scraper.rb
+++ lib/first_project/scraper.rb
@@ -14,24 +14,25 @@ class FirstProject::Scraper
     env_1 = self.new
     env_1.name = "Education"
     env_1.availability = true
-    env_1.url = "https://www.uopeople.edu/"
+    env_1.url = "https://www.upwork.com/freelance-jobs/"
 
     env_2 = self.new
     env_2.name = "Business Administration"
     env_2.availability = true
-    env_2.url = "https://www.uopeople.edu/"
+    env_2.url = "https://www.upwork.com/freelance-jobs/"
 
     env_3 = self.new
     env_3.name = "Computer Science"
     env_3.availability = true
-    env_3.url = "https://www.uopeople.edu/"
+    env_3.url = "https://www.upwork.com/freelance-jobs/"
 
     env_4 = self.new
     env_4.name = "Health Science"
     env_4.availability = true
-    env_4.url = "https://www.uopeople.edu/"
+    env_4.url = "https://www.upwork.com/freelance-jobs/"
 
     [env_1, env_2, env_3, env_4]
+    binding.pry
   end
 
 end
