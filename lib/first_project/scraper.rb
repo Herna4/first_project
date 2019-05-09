c457857e2685b3488ce8553ca0b8006a47b47278
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-09 18:16:57 +0000

    commit

--- lib/first_project/scraper.rb
+++ lib/first_project/scraper.rb
@@ -1,6 +1,6 @@
 class FirstProject::Scraper
 
-   attr_accessor :name, :available, :url
+   attr_accessor :name, :availability, :url
 
   def self.today
     #return a branch of instances of environmment
@@ -16,17 +16,22 @@ class FirstProject::Scraper
     env_1.availability = true
     env_1.url = "https://www.uopeople.edu/"
 
+    env_2 = self.new
     env_2.name = "Business Administration"
     env_2.availability = true
     env_2.url = "https://www.uopeople.edu/"
 
+    env_3 = self.new
     env_3.name = "Computer Science"
     env_3.availability = true
     env_3.url = "https://www.uopeople.edu/"
 
+    env_4 = self.new
     env_4.name = "Health Science"
     env_4.availability = true
     env_4.url = "https://www.uopeople.edu/"
+
+    [env_1, env_2, env_3, env_4]
   end
 
 end
